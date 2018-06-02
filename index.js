905f2fb0943307a67d9e64597092d30ddc53e6db
Chris Salvi <csalvi42@gmail.com>
2018-06-02 09:20:27 -0700

    adding a little js

---
+++ index.js
@@ -0,0 +1,9 @@
+ document.getElementsById("click1").addEventListener("click",); 
+
+ document.getElementsById("click2").addEventListener("click",); 
+
+ document.getElementsById("click3").addEventListener("click",); 
+
+ document.getElementsById("click4").addEventListener("click",); 
+
+ document.getElementsById("click5").addEventListener("click",); 
